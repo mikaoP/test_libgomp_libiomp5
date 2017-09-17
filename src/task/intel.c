d1ce926e356fd3c5056aa22c5b5a20106e5f75cb
Raúl Peñacoba <raul.mikaop.zelda@gmail.com>
2017-09-17 15:41:51 +0200

    support firstprivate vars in tasks

--- src/task/intel.c
+++ src/task/intel.c
@@ -4,6 +4,14 @@
 
 #include "libiomp.h"
 
+struct kmp_task_foo1 {
+	kmp_task_t task;
+	int p_a;
+	int p_b;
+	int p_c;
+	int p_d;
+};
+
 struct task_shared_args {
 	int *a;
 	int *b;
@@ -15,33 +23,53 @@ static ident_t loc1 = { .flags = KMP_IDENT_KMPC };
 static ident_t loc2 = { .flags = KMP_IDENT_KMPC | KMP_IDENT_BARRIER_IMPL };
 
 static void foo1(int *a, int *b, int *c, int *d) {
-	printf("%x %x %x %x\n", *a, *b, *c, *d);
+	*a = *b = *c = *d = 0xFF;
+	printf("%zx %zx %zx %zx\n", *a, *b, *c, *d);
+}
+
+static void omp_task_foo1_private_map(void *task, int **a, int **b, int **c, int **d) {
+	struct kmp_task_foo1 *tmp = task;
+	*a = &tmp->p_a;
+	*b = &tmp->p_b;
+	*c = &tmp->p_c;
+	*d = &tmp->p_d;
 }
 
 // I am not sure about args...
 static kmp_int32 task1(kmp_int32 thread_num, void *task) {
-	kmp_task_t *tmp = task;
-	printf("%d = %d\n", thread_num, omp_get_thread_num());
-	struct task_shared_args *tmp1 = tmp->shareds;
-	foo1(tmp1->a, tmp1->b, tmp1->c, tmp1->d);
+	int *p_a;
+	int *p_b;
+	int *p_c;
+	int *p_d;
+	//printf("%d = %d\n", thread_num, omp_get_thread_num());
+	omp_task_foo1_private_map(task, &p_a, &p_b, &p_c, &p_d);
+	foo1(p_a, p_b, p_c, p_d);
+	return 0;
 }
 
 static void outline_func(kmp_int32 *global_tid, kmp_int32 *bound_tid, int *a, int *b, int *c, int *d) {
 	if (__kmpc_single(&loc1, *global_tid)) {
-		kmp_task_t *ret1 = __kmpc_omp_task_alloc(&loc1,
+		struct kmp_task_foo1 *ret1 = (struct kmp_task_foo1 *)__kmpc_omp_task_alloc(&loc1,
                                                          *global_tid,
                                                          TASK_EXPLICIT,
-                                                         sizeof(kmp_task_t),
+                                                         sizeof(struct kmp_task_foo1),
                                                          sizeof(struct task_shared_args),
                                                          (kmp_routine_entry_t)&task1);
-		struct task_shared_args tmp = { a, b, c, d };
-		ret1->shareds = &tmp;
 
-		__kmpc_omp_task(&loc1, *global_tid, ret1);
+		struct task_shared_args *tmp = (struct task_shared_args *)(ret1->task.shareds);
+		*tmp = (struct task_shared_args) { a, b, c, d };
+
+		ret1->p_a = *(tmp->a);
+		ret1->p_b = *(tmp->b);
+		ret1->p_c = *(tmp->c);
+		ret1->p_d = *(tmp->d);
+
+		__kmpc_omp_task(&loc1, *global_tid, (kmp_task_t *)ret1);
 
 		__kmpc_end_single(&loc1, *global_tid);
 	}
 	__kmpc_barrier(&loc2, *global_tid);
+	printf("%zx %zx %zx %zx\n", *a, *b, *c, *d);
 }
 
 int main(void) {
--- test/test10.c
+++ test/test10.c
@@ -2,63 +2,23 @@
 #include <stdlib.h>
 #include <omp.h>
 
-void foo0() {
-	printf("HOLS");
-}
-
-void foo1(int *a) {
-	printf("%d", *a);
-}
-
-void foo2(int *a, int *b) {
-	printf("%d %d", *a, *b);
-}
-
-void foo3(int *a, int *b, int *c) {
-	printf("%d %d %d", *a, *b, *c);
-}
-
-void foo4(int *a, int *b, int *c, int *d) {
-	printf("%d %d %d %d", *a, *b, *c, *d);
-}
-
-void foo5(int *a, int *b, int *c, int *d, int *e) {
-	printf("%d %d %d %d %d", *a, *b, *c, *d, *e);
+void foo1(size_t *a, size_t *b, size_t *c, size_t *d) {
+	*a = *b = *c = *d = 0xFF;
+	printf("%zx %zx %zx %zx\n", *a, *b, *c, *d);
 }
 
 int main(void) {
-	int a, b, c, d, e;
-	#pragma omp parallel
+	size_t a = 0xAA;
+	size_t b = 0xBB;
+	size_t c = 0xCC;
+	size_t d = 0xDD;
+	#pragma omp parallel firstprivate(a, b, c, d)
 	{
 		#pragma omp single
 		{
 			#pragma omp task
-			foo0();
-			a = 1;
-			#pragma omp task
-			foo1(&a);
-			a = 2;
-			b = 3;
-			#pragma omp task
-			foo2(&a, &b);
-			a = 3;
-			b = 4;
-			c = 5;
-			#pragma omp task
-			foo3(&a, &b, &c);
-			a = 4;
-			b = 5;
-			c = 6;
-			d = 7;
-			#pragma omp task
-			foo4(&a, &b, &c, &d);
-			a = 5;
-			b = 6;
-			c = 7;
-			d = 8;
-			e = 9;
-			#pragma omp task
-			foo5(&a, &b, &c, &d, &e);
+			foo1(&a, &b, &c, &d);
 		}
+		printf("%zx %zx %zx %zx\n", a, b, c, d);
 	}
 }
